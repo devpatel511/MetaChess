b9b5b597f06c9193d0ce7728c2c5ef0eb02e8e47
Dev Patel <104709171+devpatel511@users.noreply.github.com>
2023-01-09 01:28:15 -0500

    Creating Routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,34 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
+import Navbar from "./UI/navbar";
 import Board from './UI/board';
+import Users from './UI/users';
 
 // const API_BASE = "http://localhost:3001"
 
 function App() {
   return (
-	<div className="App">
-		<Board>
-
-		</Board>
-		<div className="moves">
-			<h1>Moves</h1>
-			<p>e4</p>
-			<p>e5</p>
-		</div>
-	</div>
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<Users/>}/>
+			{/* <Navbar />
+			<br/>
+			<Route path="/" exact>
+				<Board></Board>
+			</Route> */}
+			{/* <Route path="/edit/:id" />
+			<Route path="/create"  />
+			<Route path="/user">
+				<Board></Board>
+			</Route> */}
+			{/* <div className="moves">
+				<h1>Moves</h1>
+				<p>e4</p>
+				<p>e5</p>
+			</div> */}
+		</Routes>
+	</BrowserRouter>
   );
 }
 
---
+++ front-end/src/UI/users.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Users extends Component {
+    render() {
+        return (
+            <h1>12345</h1>
+        )
+    }
+}
