8cc60c69a2d91a5904a674b05e36e54190e6bb30
Dev Patel <104709171+devpatel511@users.noreply.github.com>
2023-01-16 01:12:38 -0500

    Navbar changes

--- front-end/src/components/navbar.js
+++ front-end/src/components/navbar.js
@@ -5,24 +5,27 @@ export default class Navbar extends Component {
 
   render() {
 	return (
-	  <>
-	  <nav>
-		<ul>
-		  <li>
-			<Link to="/" className='link active'>HOME</Link>
-		  </li>
-		  <li>
-			<Link to="/game" className='link'>PLAY</Link>
-		  </li>
-		  <li>
-			<Link to="/login" className='link'>LOGIN</Link>
-		  </li>
-		  <li>
-			<Link to="/stats" className='link'>STATS</Link>
-		  </li>
-		</ul>
-	  </nav>
-	</>
+		<>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/" className='link'>MetaChess</Link>
+					</li>
+					<li>
+						<Link to="/" className='link active'>HOME</Link>
+					</li>
+					<li>
+						<Link to="/game" className='link'>PLAY</Link>
+					</li>
+					<li>
+						<Link to="/login" className='link'>LOGIN</Link>
+					</li>
+					<li>
+						<Link to="/stats" className='link'>STATS</Link>
+					</li>
+				</ul>
+			</nav>
+		</>
 	);
   }
 }
