4d17120d5b0b8f9a1768f448848642cc775f8a82
Dev Patel <104709171+devpatel511@users.noreply.github.com>
2023-01-17 01:18:11 -0500

    Adding extra modes

--- front-end/src/Model/game.js
+++ front-end/src/Model/game.js
@@ -4,6 +4,10 @@ function Game() {
     return (
         <>
             <Link to="/game/local">Play Local</Link>
+            <br/>
+            <Link to="game/online">Play Online</Link>
+            <br/>
+            <Link to="game/computer">Play Computer</Link>
         </>
     )
 }
