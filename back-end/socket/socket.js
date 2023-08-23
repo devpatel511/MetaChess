efd61330b8b6e62d58c049bc00a395864cbde603
Dev Patel <104709171+devpatel511@users.noreply.github.com>
2023-08-23 00:07:22 -0400

    Added website link to cors

--- back-end/socket/socket.js
+++ back-end/socket/socket.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "https://fancy-lokum-132358.netlify.app/",
+        origin: ['http://localhost:3000', 'https://moonlit-cannoli-1ee65d.netlify.app/'],
         methods: ["GET", "POST"],
     },
 })
